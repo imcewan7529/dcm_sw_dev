6f6df90c3d36483888e4b81d04eb479bd8477b4e
sxm0603 <smuayad5857@sdsu.edu>
2023-10-30 15:14:30 -0700

    testing issue of stuck in consuming

--- JSON_assembler/src/json_assembler.py
+++ JSON_assembler/src/json_assembler.py
@@ -28,42 +28,42 @@ channel.queue_declare(queue=queue_name)
 # Consume from queue
 channel.basic_consume(queue=queue_name, auto_ack=True, on_message_callback=callback)
 
-try:
-    print("Started consuming messages")
-    channel.start_consuming()
+while True:
+    try:
+        print("Started consuming messages")
+        channel.start_consuming()
 
-except KeyboardInterrupt:
-    channel.stop_consuming()
-    connection.close()
+        # Put data into python dictionary
+        try:
+            data_dict = json.loads(data_received)
+            print(f"Data as a dictionary: {data_dict}")
 
-# ************************************************************************************
-# Put data into python dictionary
-try:
-    data_dict = json.loads(data_received)
-    print(f"Data as a dictionary: {data_dict}")
+        except json.JSONDecodeError as e:
+            print("Failed to parse as JSON")
 
-except json.JSONDecodeError as e:
-    print("Failed to parse as JSON")
+        else:
+            print("Successfully parsed as JSON")
 
-else:
-    print("Successfully parsed as JSON")
+        # Write as a file
+        try:
+            while True:
+                current_time = datetime.datetime.now().strftime("%Y/%m/%d_%H:%M:%S")
+                new_file = os.path.join('/tmp/', f'{current_time}.json')
 
-# Write as a file
-try:
-    while True:
-        current_time = datetime.datetime.now().strftime("%Y/%m/%d_%H:%M:%S")
-        new_file = os.path.join('/tmp/', f'{current_time}.json')
+                with open(new_file, 'w') as json_file:
+                    json.dump(data_dict, json_file)
 
-        with open(new_file, 'w') as json_file:
-            json.dump(data_dict, json_file)
+        except FileNotFoundError:
+            print("File not found")
 
-except FileNotFoundError:
-    print("File not found")
+        except PermissionError:
+            print("Does not have permission to access file")
 
-except PermissionError:
-    print("Does not have permission to access file")
+        except Exception as e:
+            print("Error writing to JSON file")
 
-except Exception as e:
-    print("Error writing to JSON file")
+        time.sleep(30)
 
-time.sleep(30)
+    except KeyboardInterrupt:
+        channel.stop_consuming()
+        connection.close()
