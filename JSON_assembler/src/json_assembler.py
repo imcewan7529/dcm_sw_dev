d87fb7266d3560f2a2d8c2f030b2e7cad6802f7a
sxm0603 <smuayad5857@sdsu.edu>
2023-10-30 10:58:44 -0700

    jsonAssembler completed

--- JSON_assembler/src/json_assembler.py
+++ JSON_assembler/src/json_assembler.py
@@ -1,7 +1,14 @@
 import pika
 import json
+import time
+import datetime
+import tempfile
+import os
 
 # ************************************************************************************
+# Global variables
+data_received = None
+
 # Read data from queue
 # Callback function
 def callback(ch, method, properties, body):
@@ -15,26 +22,49 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 # Create a channel
 channel = connection.channel()
 
-# Connect to queue (name is placeholder for given name by ADC)
+# Connect to queue
 queue_name = 'ADCname'
 channel.queue_declare(queue=queue_name)
 
 # Consume from queue
 channel.basic_consume(queue=queue_name, auto_ack=True, on_message_callback=callback)
 
-print("Started consuming messages")
+try:
+    print("Started consuming messages")
+    channel.start_consuming()
 
-channel.start_consuming()
+except KeyboardInterrupt:
+    channel.stop_consuming()
+    connection.close()
+
 # ************************************************************************************
-# Convert data to json
-
-
 # Put data into python dictionary
-data_dict = json.loads(data_received_str)
+try:
+    data_dict = json.loads(data_received)
+    print(f"Data as a dictionary: {data_dict}")
 
-# Format as JSON
-data_json = json.dumps(data_dict, indent=4)
+except json.JSONDecodeError as e:
+    print("Failed to parse as JSON")
+
+else:
+    print("Successfully parsed as JSON")
 
 # Write as a file
-with open('data_out.json', 'w') as json_file:
-    json_file.write(data_json)
+try:
+    while True:
+        current_time = datetime.datetime.now().strftime("%Y/%m/%d_%H:%M:%S")
+        new_file = os.path.join('/tmp/', f'{current_time}.json')
+
+        with open(new_file, 'w') as json_file:
+            json.dump(data_dict, json_file)
+
+except FileNotFoundError:
+    print("File not found")
+
+except PermissionError:
+    print("Does not have permission to access file")
+
+except Exception as e:
+    print("Error writing to JSON file")
+
+time.sleep(30)
