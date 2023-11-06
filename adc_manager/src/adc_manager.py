12cb4330405a5149e62898a76d3d98991b8d2944
jdo1156 <127704857+jdo1156@users.noreply.github.com>
2023-11-05 17:27:35 -0800

    Added code to publish data on queue

--- adc_manager/src/adc_manager.py
+++ adc_manager/src/adc_manager.py
@@ -1,16 +1,18 @@
 from MCP3008 import MCP3008
 from datetime import datetime, timezone
 from lowpass_filter import Lowpass_Filter
-import json  
+
+import json
+import pika
 import time
 
 # adc driver
 adc = MCP3008()
 
 # log file  (will be deleted later) 
-filename = '../test_logs/log1hz_RC_Digital.json'              
-log_file = open(filename, 'r+') 
-file_data = json.load(log_file)
+#filename = '../test_logs/log1hz_RC_Digital.json'              
+#log_file = open(filename, 'r+') 
+#file_data = json.load(log_file)
 
 # writes data to json file (will be moved to Json generator later)
 def write_json(new_data, write_file):
@@ -18,6 +20,20 @@ def write_json(new_data, write_file):
     write_file.seek(0)
     json.dump(file_data, write_file, indent=4,sort_keys=True, default=str)
 
+# Callback function
+def callback(ch, method, properties, body):
+    global data_received
+    data_received = body.decode('utf-8')
+    print(f"Message received: {data_received}")
+# Create connection to MQ
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+
+# Create a channel
+channel = connection.channel()
+
+# Connect to queue
+queue_name = 'DCM_Main_Exchange'
+channel.queue_declare(queue=queue_name)
 
 def main():
     p1 = Lowpass_Filter()
@@ -31,11 +47,15 @@ def main():
         #Formats data to json
         fields = {"Date/Time": now, "Voltage": filtered_value}
         #write_json(fields, log_file) 
+        channel.basic_publish(exchange='',
+                      routing_key='queue_name',
+                      body= fields)
         print("Date: %s | Voltage: %.4f"%  (now , filtered_value) )
         # Delaysample
         time.sleep(-(time.time() - t0)% .1)        
 
-    log_file.close()
+    connection.close()
+    #log_file.close()
 
 
 if __name__ == "__main__":
